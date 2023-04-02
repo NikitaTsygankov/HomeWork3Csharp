3b98a79c472eaa74336b30e61c8d6be1e73a56b3
Nikita Tsygankov <like_fender@mail.ru>
2023-04-02 23:26:23 +0300

    task 1 added

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит 
+//расстояние между ними в 3D пространстве.
+
+Number(Read());
+
+int Read()
+{
+    Console.WriteLine("Введите число:");
+    int N = int.Parse(Console.ReadLine());
+    return N;
+}
+
+void Number(int N)
+{
+    int n = 1;
+    while (n <= N)
+    {
+        double d = Math.Pow(n, 3);
+        Console.Write(d+" ");
+        n++;
+    }
+}
+
+
